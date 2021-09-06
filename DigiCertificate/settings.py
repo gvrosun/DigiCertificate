37d50389d3f96dcbb961a8decb91b4959fa947e8
Rosun GV <gvrosun@gmail.com>
2021-09-06 19:36:21 +0530

    Deployment

--- DigiCertificate/settings.py
+++ DigiCertificate/settings.py
@@ -11,20 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+django_heroku.settings(locals())
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)rr2!p*esinuw^zsva7pe7%)^e%ip^(kpa-6g=3cc_g)%b-7jf'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.gvrosun.me', 'gvrosun.me']
 
 # Application definition
 
