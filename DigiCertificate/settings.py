46681547d8e9ee1a77fed7076a9875e1e819addc
Rosun GV <gvrosun@gmail.com>
2021-09-10 09:50:05 +0530

    Updated DEBUG

--- DigiCertificate/settings.py
+++ DigiCertificate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='abc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', default=True)
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['www.gvrosun.me', 'gvrosun.me']
 
